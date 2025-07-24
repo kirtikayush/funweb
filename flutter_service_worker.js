a4d57f1c90d01c50bf3ffc1a38718bf06feff6b8
Kirtik Ayush <kirtikayush2000@gmail.com>
2025-07-25 03:43:49 +0530

    funwebite joke improved + html

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "d1e588fba43dac110b72a8f743e6089c",
+const RESOURCES = {"flutter_bootstrap.js": "5e342ddcb42819e65d18ad1398e06e14",
 "version.json": "4b1bb4872956afb0d5de61908267f09c",
-"index.html": "a36b364131a939208e3d7abdab2cc83b",
-"/": "a36b364131a939208e3d7abdab2cc83b",
-"main.dart.js": "eb9d882699cd0dc0650e5e751685aaf0",
+"index.html": "df237d90302c45a1fcaccfe9c37847cb",
+"/": "df237d90302c45a1fcaccfe9c37847cb",
+"main.dart.js": "bb64d37dc65d4f2b2ec5c402668b824e",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
