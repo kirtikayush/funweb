2b497d2efe69f3dfd10ff4e1da1a75a93bac3e5b
Kirtik Ayush <kirtikayush2000@gmail.com>
2025-07-25 01:46:04 +0530

    funwebite joke basic added

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "269352fd35d1fd90b30f5f6a586e5015",
+const RESOURCES = {"flutter_bootstrap.js": "d1e588fba43dac110b72a8f743e6089c",
 "version.json": "4b1bb4872956afb0d5de61908267f09c",
 "index.html": "a36b364131a939208e3d7abdab2cc83b",
 "/": "a36b364131a939208e3d7abdab2cc83b",
-"main.dart.js": "ab418559c95bc709b2e7836797979f41",
+"main.dart.js": "eb9d882699cd0dc0650e5e751685aaf0",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
